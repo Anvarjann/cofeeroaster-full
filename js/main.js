f95440185e3dd1808bdef67650d841f6ec990f9c
Anvarjann <anvarochilov977@gmail.com>
2022-08-25 11:59:21 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let elBtnOpen = document.querySelector(".open-btn")
+let elNavShow = document.querySelector(".site-header__nav-show")
+let elOverlayshow = document.querySelector(".overlay")
+let elBody = document.querySelector("body")
+
+elBtnOpen.addEventListener("click", function(){
+    elNavShow.classList.toggle("site-header__nav-show-active")
+    elBtnOpen.classList.toggle("open-btn-show")
+    elOverlayshow.classList.toggle("overlay-show")
+    elBody.classList.toggle("body-active")
+})
+elOverlayshow.addEventListener("click", function(){
+    elBtnOpen.classList.remove("open-btn-show")
+    elNavShow.classList.remove("site-header__nav-show-active")
+    elOverlayshow.classList.remove("overlay-show")
+    elBody.classList.remove("body-active")
+})
